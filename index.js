d6cc87a390d6fac08fca7c997a5858f4af4e5f35
QuanKhanz <iamhoangkhang@icloud.com>
2024-02-04 11:30:29 +0700

    8. Install Morgan

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/trang-chu', (req, res) => {
+
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 
 app.listen(port, () => {
