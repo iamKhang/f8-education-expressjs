c812cb582902a05189b1a8b57925438a88120ba9
QuanKhanz <iamhoangkhang@icloud.com>
2024-02-11 17:20:35 +0700

    9. Template engine (handlebars)

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const handlebars  = require('express-handlebars');
+const port = 3000
+const app = express()
+
+
+// http logger
+app.use(morgan('combined'))
+
+
+// template engines
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+
+
+app.get('/', (req, res) => {
+  res.render('news')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
