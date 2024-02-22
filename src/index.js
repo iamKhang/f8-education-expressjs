26cf9a56f4b17bd9bb2868ef481e5adf4c530f02
QuanKhanz <iamhoangkhang@icloud.com>
2024-02-22 11:04:05 +0700

    GET + Query parameters + Form default behavior + POST

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded(
+ {extended: true}
+))
+app.use(express.json())
 
 // http logger
 app.use(morgan('combined'))
@@ -22,9 +26,21 @@ app.set('views', path.join(__dirname,'resources/views'));
 
 
 app.get('/', (req, res) => {
-  res.render('news')
+  res.render('home')
 })
 
+app.get('/news', (request, response) => {
+  response.render('news')
+})
+
+app.get('/search', (request, response) => {
+  response.render('search')
+})
+
+app.post('/search', (request, response) => {
+  console.log(request.body)
+  response.send("Chào")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
