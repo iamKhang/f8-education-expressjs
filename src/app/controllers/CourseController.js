e000d989510a21ba96bd594c2b4691af8caaaf1b
QuanKhanz <iamhoangkhang@icloud.com>
2024-02-27 22:02:31 +0700

    30. [CRUD] Delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -48,6 +48,12 @@ class CourseController {
     .then(() => res.redirect('/me/stored/courses'))
     .catch(next)
   }
+
+  delete(req, res, next){
+    Course.deleteOne({_id: req.params.id}, req.body)
+    .then(()=> res.redirect('back'))
+    .catch(next)
+  }
 }
 
 module.exports = new CourseController();
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,14 +1,13 @@
 // Using Node.js `require()`
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-async function connect(){
-    try {
-        await mongoose.connect('mongodb://localhost:27017/f8_education_dev');
-        console.log("Kết nối thành công")
-    } catch (error) {
-        console.log("Kết nối không thành công")
-    }
+async function connect() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/f8_education_dev");
+    console.log("Kết nối thành công");
+  } catch (error) {
+    console.log("Kết nối không thành công");
+  }
 }
 
-module.exports = {connect}
+module.exports = { connect };
